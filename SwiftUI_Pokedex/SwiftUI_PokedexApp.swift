9d484147ab676a04dfec84db6ce6032c54e3a58b
Tariyel <islami.tariyel@gmail.com>
2023-02-17 17:43:56 +0400

    Initial commit

---
+++ SwiftUI_Pokedex/SwiftUI_PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PokedexApp.swift
+//  SwiftUI_Pokedex
+//
+//  Created by Tariyel Islami on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
